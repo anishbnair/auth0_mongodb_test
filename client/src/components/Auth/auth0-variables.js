6a0e0a4c16a47d66f60f398cce492fca53c636cb
anishbnair <nbanish@gmail.com>
2018-04-25 16:22:00 -0400

    testing

---
+++ client/src/components/Auth/auth0-variables.js
@@ -0,0 +1,16 @@
+export const AUTH_CONFIG = {
+    // domain: 'project-3.auth0.com',
+    // clientId: 'a6R3KKabd2LumdWZAafpqffgbUmeQrce',
+    // callbackUrl: 'http://localhost:3000/callback'
+
+
+    // domain: 'app94688708.auth0.com',
+    // clientId: 'lRJjwPhtHxwATPLCUVZpwKbl1Sj20rd6',
+    // callbackUrl: 'https://mentor-match-learn2code.herokuapp.com/callback'
+
+    domain: 'mentor-match.auth0.com',
+    clientId: 'srfL4vr5SB0gSsS8yne5OQxJjhWFKMKc',
+    callbackUrl: 'http://localhost:3000/callback'
+    // callbackUrl: 'https://mentor-match-learn2code.herokuapp.com/callback'
+
+}
